ee0ed67190e6ad77b8afc28fcf3070195f7f08d6
Sergey Zubov <zubov@spb.gs.ru>
2016-04-09 22:30:19 +0300

    implement index_of algorithm for parameter packs

---
+++ jbi/algorithm/pp.h
@@ -0,0 +1,39 @@
+#ifndef JBI_ALGORITHM_PP_H
+#define JBI_ALGORITHM_PP_H
+
+#include <type_traits>
+
+namespace jbi
+{
+
+    // pp stands for 'parameter pack'
+    namespace pp
+    {
+
+        constexpr std::size_t noindex = std::numeric_limits<std::size_t>::max();
+
+        namespace detail
+        {
+
+            template < std::size_t Index, typename T, typename... Ts >
+            struct index_of_impl;
+
+            template < std::size_t Index, typename T, typename Head, typename... Tail>
+            struct index_of_impl<Index, T, Head, Tail...> : std::integral_constant<std::size_t, std::is_same<T, Head>::value? Index : index_of_impl<Index + 1, T, Tail...>::value>
+            { };
+
+            template < std::size_t Index, typename T, typename Last >
+            struct index_of_impl<Index, T, Last> : std::integral_constant<std::size_t, std::is_same<T, Last>::value? Index : noindex>
+            { };
+
+        }
+
+        template < typename T, typename... Ts >
+        struct index_of : std::integral_constant<std::size_t, detail::index_of_impl<0, T, Ts...>::value>
+        { };
+
+    }
+
+}
+
+#endif
