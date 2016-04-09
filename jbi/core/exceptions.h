025a82a1db8d09a0cad72d067045d7c91a135e6c
Sergey Zubov <zubov@spb.gs.ru>
2016-04-09 14:19:39 +0300

    add not_implemented_error exception

--- jbi/core/exceptions.h
+++ jbi/core/exceptions.h
@@ -1,8 +1,22 @@
 #ifndef JBI_CORE_EXCEPTIONS_H
 #define JBI_CORE_EXCEPTIONS_H
 
+#include <stdexcept>
+
 // TODO store context (__FILE__, __LINE__, etc.)
 #define JBI_THROW(...) \
     throw __VA_ARGS__
+
+namespace jbi
+{
+
+    class not_implemented_error : std::runtime_error
+    {
+    public:
+        not_implemented_error() : std::runtime_error("The method or function is not implemented")
+        { }
+    };
+
+}
 
 #endif
