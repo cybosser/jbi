c4d1ad9db53d61c026513cfaaba81dedebb17a1d
Sergey Zubov <zubov@spb.gs.ru>
2016-04-09 17:39:46 +0300

    add greeting output, read statements from console

--- ijbi/main.cpp
+++ ijbi/main.cpp
@@ -1,13 +1,28 @@
+#include <ijbi/io/console.h>
+
 #include <jbi/interpreter/interpreter.h>
 
 #include <cstdlib>
 
 int main()
 {
+    ijbi::console console;
+
+    console.write_line("Welcome to ijbi, an interactive interpreter");
+    console.write_line("Type 'exit' to exit");
+
+    jbi::interpreter interpreter;
+
     try
     {
-        jbi::interpreter interpreter;
-        interpreter.interpret("2+2");
+        while (true)
+        {
+            const std::string statement = console.read_line();
+            if (statement == "exit")
+                return EXIT_SUCCESS;
+
+            interpreter.interpret(statement);
+        }
     }
     catch (const std::exception& ex)
     {
