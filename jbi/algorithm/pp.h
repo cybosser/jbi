d9a4d8a3c7fc505a5cd75db7a0d0abd39e36b420
Sergey Zubov <cybosser@gmail.com>
2016-04-10 20:52:52 +0300

    simplify implementation of pp::index_of

--- jbi/algorithm/pp.h
+++ jbi/algorithm/pp.h
@@ -1,7 +1,7 @@
 #ifndef JBI_ALGORITHM_PP_H
 #define JBI_ALGORITHM_PP_H
 
-#include <type_traits>
+#include <jbi/core/type_traits.h>
 
 namespace jbi
 {
@@ -19,18 +19,17 @@ namespace jbi
             struct index_of_impl;
 
             template < std::size_t Index, typename T, typename Head, typename... Tail>
-            struct index_of_impl<Index, T, Head, Tail...> : std::integral_constant<std::size_t, std::is_same<T, Head>::value? Index : index_of_impl<Index + 1, T, Tail...>::value>
+            struct index_of_impl<Index, T, Head, Tail...> : conditional_t<std::is_same<T, Head>::value, size_t_constant<Index>, index_of_impl<Index + 1, T, Tail...>>
             { };
 
             template < std::size_t Index, typename T, typename Last >
-            struct index_of_impl<Index, T, Last> : std::integral_constant<std::size_t, std::is_same<T, Last>::value? Index : npos>
+            struct index_of_impl<Index, T, Last> : conditional_t<std::is_same<T, Last>::value, size_t_constant<Index>, size_t_constant<npos>>
             { };
 
         }
 
         template < typename T, typename... Ts >
-        struct index_of : std::integral_constant<std::size_t, detail::index_of_impl<0, T, Ts...>::value>
-        { };
+        using index_of = detail::index_of_impl<0, T, Ts...>;
 
     }
 
