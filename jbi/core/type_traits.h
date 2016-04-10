45bd138adb83fea148de4b4b581170359e4bcb6e
Sergey Zubov <cybosser@gmail.com>
2016-04-10 20:46:03 +0300

    add conditional_t alias

--- jbi/core/type_traits.h
+++ jbi/core/type_traits.h
@@ -9,6 +9,9 @@ namespace jbi
     template < std::size_t Length, typename... Ts >
     using aligned_union_t = typename std::aligned_union<Length, Ts...>::type;
 
+    template < bool Condition, typename T, typename F >
+    using conditional_t = typename std::conditional<Condition, T, F>::type;
+
     template < bool Condition, typename T = void >
     using enable_if_t = typename std::enable_if<Condition, T>::type;
 
