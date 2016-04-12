3453761e4c464d8fd41f1be9041cb9647a532c8d
Sergey Zubov <zubov@spb.gs.ru>
2016-04-12 18:00:27 +0300

    add test for variant::apply_visitor method

--- jbi_tests/core_tests/variant_tests.cpp
+++ jbi_tests/core_tests/variant_tests.cpp
@@ -29,6 +29,46 @@ TEST(core_variant_tests, destructor_test)
     EXPECT_TRUE(destructor_called);
 };
 
+TEST(core_variant_tests, apply_visitor_test)
+{
+    static constexpr int expected_int = 1948;
+    static constexpr float expected_float = 3.14f;
+
+    enum class number_type
+    {
+        int_, float_
+    };
+
+    struct visitor : public jbi::static_visitor<number_type>
+    {
+        number_type operator()(int value)
+        {
+            EXPECT_EQ(expected_int, value);
+            return number_type::int_;
+        }
+
+        number_type operator()(float value)
+        {
+            EXPECT_EQ(expected_float, value);
+            return number_type::float_;
+        }
+    };
+
+    visitor visitor;
+
+    jbi::variant<int, float> int_variant(expected_int);
+    EXPECT_EQ(number_type::int_, int_variant.apply_visitor(visitor));
+
+    jbi::variant<int, float> float_variant(expected_float);
+    EXPECT_EQ(number_type::float_, float_variant.apply_visitor(visitor));
+
+    const jbi::variant<int, float> const_int_variant(expected_int);
+    EXPECT_EQ(number_type::int_, const_int_variant.apply_visitor(visitor));
+
+    const jbi::variant<int, float> const_float_variant(expected_float);
+    EXPECT_EQ(number_type::float_, const_float_variant.apply_visitor(visitor));
+}
+
 TEST(core_variant_tests, get_test)
 {
     jbi::variant<int, float> int_variant(1984);
