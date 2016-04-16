3c1ed7db89414cbcb8d5309709986b5807bb6a7b
Sergey Zubov <cybosser@gmail.com>
2016-04-16 21:26:41 +0300

    implement lexer

--- jbi/interpreter/lexer.cpp
+++ jbi/interpreter/lexer.cpp
@@ -1,17 +1,34 @@
 #include <jbi/interpreter/lexer.h>
 
+#include <jbi/interpreter/lexer/flex/scanner.h>
+
 namespace jbi
 {
 
     class lexer::impl
     {
+    private:
+        yyscan_t        _scanner;
+        YY_BUFFER_STATE _buffer;
+
     public:
         impl(const std::string& statement)
-        { }
+        {
+            JBI_THROW_IF(yylex_init(&_scanner) != 0, std::runtime_error("yylex_init failed!"));
+
+            _buffer = yy_scan_bytes(statement.data(), statement.length(), _scanner);
+        }
+
+        ~impl()
+        {
+            yy_delete_buffer(_buffer, _scanner);
+
+            yylex_destroy(_scanner);
+        }
 
         token read_token()
         {
-            JBI_THROW(not_implemented_exception());
+            return yylex(_scanner);
         }
     };
 
---
+++ jbi_tests/interpreter_tests/lexer_tests/lexer_tests.cpp
@@ -0,0 +1,79 @@
+#include <gtest/gtest.h>
+
+#include <jbi/interpreter/lexer.h>
+
+class lexer_tester
+{
+private:
+    jbi::lexer  _lexer;
+
+public:
+    explicit lexer_tester(const std::string& statement)
+        : _lexer(statement)
+    { }
+
+    ~lexer_tester()
+    {
+        expect(jbi::token::eof());
+    }
+
+    void expect(const jbi::token& expected)
+    {
+        EXPECT_EQ(expected, _lexer.read_token());
+    }
+};
+
+TEST(lexer_tests, keywords_test)
+{
+    lexer_tester tester("var in out print");
+
+    tester.expect(jbi::token::var());
+    tester.expect(jbi::token::in());
+    tester.expect(jbi::token::out());
+    tester.expect(jbi::token::print());
+}
+
+TEST(lexer_tests, identifiers_test)
+{
+    lexer_tester tester("a foo b2");
+
+    tester.expect(jbi::token::identifier("a"));
+    tester.expect(jbi::token::identifier("foo"));
+    tester.expect(jbi::token::identifier("b2"));
+}
+
+TEST(lexer_tests, numbers_test)
+{
+    lexer_tester tester("7 42 1984 3.14 0.5 9.");
+
+    tester.expect(jbi::token::number(7));
+    tester.expect(jbi::token::number(42));
+    tester.expect(jbi::token::number(1984));
+    tester.expect(jbi::token::number(3.14));
+    tester.expect(jbi::token::number(0.5));
+    tester.expect(jbi::token::number(9.0));
+}
+
+TEST(lexer_tests, symbols_test)
+{
+    lexer_tester tester("= , + - / * ^ {} ( )");
+
+    tester.expect(jbi::token::equals());
+    tester.expect(jbi::token::comma());
+    tester.expect(jbi::token::plus());
+    tester.expect(jbi::token::minus());
+    tester.expect(jbi::token::slash());
+    tester.expect(jbi::token::asterisk());
+    tester.expect(jbi::token::carret());
+    tester.expect(jbi::token::left_brace());
+    tester.expect(jbi::token::right_brace());
+    tester.expect(jbi::token::left_parenthesis());
+    tester.expect(jbi::token::right_parenthesis());
+}
+
+TEST(lexer_tests, arrow_test)
+{
+    lexer_tester tester("->");
+
+    tester.expect(jbi::token::arrow());
+}
