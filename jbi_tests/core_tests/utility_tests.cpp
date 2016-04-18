d65d44e55455e142a6fd14609ed7b25fcfa19187
Sergey Zubov <cybosser@gmail.com>
2016-04-18 12:58:47 +0300

    add is_one_of test

---
+++ jbi_tests/core_tests/utility_tests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include <jbi/core/utility.h>
+
+TEST(core_utility_tests, is_one_of_test)
+{
+    EXPECT_TRUE(jbi::is_one_of('a', 'a', 'b', 'c'));
+    EXPECT_TRUE(jbi::is_one_of('b', 'a', 'b', 'c'));
+    EXPECT_TRUE(jbi::is_one_of('c', 'a', 'b', 'c'));
+    EXPECT_FALSE(jbi::is_one_of('d', 'a', 'b', 'c'));
+}
