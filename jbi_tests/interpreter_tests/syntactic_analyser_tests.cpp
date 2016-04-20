1614ee519027d32eedb321fe23a9806bd1d7ba50
Sergey Zubov <cybosser@gmail.com>
2016-04-20 12:44:23 +0300

    add syntactic_analyser tests

---
+++ jbi_tests/interpreter_tests/syntactic_analyser_tests.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include <jbi/interpreter/syntactic_analyser.h>
+
+std::string parse(const std::string& statement)
+{
+    // TODO mock lexical_analyser
+    jbi::syntactic_analyser parser{ jbi::lexical_analyser(statement) };
+    return jbi::to_string(*parser.parse());
+}
+
+TEST(syntactic_analyser_tests, assignment_statement_test)
+{
+    EXPECT_EQ("id:foo = const:1", parse("var foo = 1"));
+
+    EXPECT_THROW(parse("var * = 1"), jbi::syntax_exception);
+    EXPECT_THROW(parse("var 1 = 1"), jbi::syntax_exception);
+    EXPECT_THROW(parse("var out = 1"), jbi::syntax_exception);
+}
+
+TEST(syntactic_analyser_tests, associativity_test)
+{
+    EXPECT_EQ("id:foo = ((const:1 + const:2) - const:3)", parse("var foo = 1 + 2 - 3"));
+    EXPECT_EQ("id:foo = ((const:1 - const:2) + const:3)", parse("var foo = 1 - 2 + 3"));
+
+    EXPECT_EQ("id:foo = ((const:1 * const:2) / const:3)", parse("var foo = 1 * 2 / 3"));
+    EXPECT_EQ("id:foo = ((const:1 / const:2) * const:3)", parse("var foo = 1 / 2 * 3"));
+}
