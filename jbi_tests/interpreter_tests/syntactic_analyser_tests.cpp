328d3260f433587cecb753446fa245d69202d737
Sergey Zubov <zubov@spb.gs.ru>
2016-04-22 17:35:54 +0300

    remove unnecessary local variable

--- jbi_tests/interpreter_tests/syntactic_analyser_tests.cpp
+++ jbi_tests/interpreter_tests/syntactic_analyser_tests.cpp
@@ -6,23 +6,23 @@
 
 struct lispifier : public jbi::syntax_tree_visitor<std::string>
 {
-    std::string operator()(const jbi::arithmetic_operator& op)
+    std::string operator()(const jbi::arithmetic_operator& op) const
     {
         return "(" + jbi::to_string(op.operation()) + " " + jbi::accept_visitor(*this, *op.left()) + " " + jbi::accept_visitor(*this, *op.right()) + ")";
     }
 
-    std::string operator()(const jbi::output_statement& out)
+    std::string operator()(const jbi::output_statement& out) const
     {
         return "(out " + jbi::accept_visitor(*this, *out.value()) + ")";
     }
 
-    std::string operator()(const jbi::assignment_statement& var)
+    std::string operator()(const jbi::assignment_statement& var) const
     {
         return "(var " + var.identifier() + " " + jbi::accept_visitor(*this, *var.initializer()) + ")";
     }
 
     template < typename T >
-    std::string operator()(const jbi::numeric_literal<T>& literal)
+    std::string operator()(const jbi::numeric_literal<T>& literal) const
     {
         return jbi::to_string(literal.value());
     }
@@ -33,9 +33,7 @@ std::string lispify(const std::string& statement)
 {
     // TODO mock lexical_analyser
     jbi::syntactic_analyser parser{ jbi::lexical_analyser(statement) };
-
-    lispifier lispifier_;
-    return jbi::accept_visitor(lispifier_, *parser.parse());
+    return jbi::accept_visitor(lispifier(), *parser.parse());
 }
 
 TEST(syntactic_analyser_tests, assignment_statement_test)
