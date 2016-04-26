58dad4fd2db89190f930b4440a7791f6cae59457
Sergey Zubov <zubov@spb.gs.ru>
2016-04-26 12:04:22 +0300

    symplify arithmetic_operation's tests

--- jbi_tests/interpreter_tests/arithmetic_operation_tests.cpp
+++ jbi_tests/interpreter_tests/arithmetic_operation_tests.cpp
@@ -2,73 +2,71 @@
 
 #include <jbi/interpreter/arithmetic_operation.h>
 
-TEST(arithmetic_operation_tests, apply_test)
+class arithmetic_operation_tests : public ::testing::Test
 {
-    jbi::arithmetic_operation add = jbi::arithmetic_operation::addition();
+protected:
+    jbi::arithmetic_operation addition = jbi::arithmetic_operation::addition();
+    jbi::arithmetic_operation subtraction = jbi::arithmetic_operation::subtraction();
+    jbi::arithmetic_operation multiplication = jbi::arithmetic_operation::multiplication();
+    jbi::arithmetic_operation division = jbi::arithmetic_operation::division();
+    jbi::arithmetic_operation exponentiation = jbi::arithmetic_operation::exponentiation();
+};
 
-    EXPECT_EQ(10, add.apply(7, 3));
-    EXPECT_DOUBLE_EQ(4.76, add.apply(3.14, 1.62));
-    EXPECT_DOUBLE_EQ(8.62, add.apply(7, 1.62));
+TEST_F(arithmetic_operation_tests, apply_test)
+{
+    EXPECT_EQ(10, addition.apply(7, 3));
+    EXPECT_DOUBLE_EQ(4.76, addition.apply(3.14, 1.62));
+    EXPECT_DOUBLE_EQ(8.62, addition.apply(7, 1.62));
 
-    jbi::arithmetic_operation subtract = jbi::arithmetic_operation::subtraction();
+    EXPECT_EQ(4, subtraction.apply(7, 3));
+    EXPECT_DOUBLE_EQ(1.52, subtraction.apply(3.14, 1.62));
+    EXPECT_DOUBLE_EQ(5.38, subtraction.apply(7, 1.62));
 
-    EXPECT_EQ(4, subtract.apply(7, 3));
-    EXPECT_DOUBLE_EQ(1.52, subtract.apply(3.14, 1.62));
-    EXPECT_DOUBLE_EQ(5.38, subtract.apply(7, 1.62));
+    EXPECT_EQ(21, multiplication.apply(7, 3));
+    EXPECT_DOUBLE_EQ(1.57, multiplication.apply(3.14, 0.5));
+    EXPECT_DOUBLE_EQ(10.5, multiplication.apply(7, 1.5));
 
-    jbi::arithmetic_operation multiply = jbi::arithmetic_operation::multiplication();
+    EXPECT_EQ(3, division.apply(7, 2));
+    EXPECT_DOUBLE_EQ(3.0, division.apply(7.5, 2.5));
+    EXPECT_DOUBLE_EQ(3.5, division.apply(7.0, 2));
 
-    EXPECT_EQ(21, multiply.apply(7, 3));
-    EXPECT_DOUBLE_EQ(1.57, multiply.apply(3.14, 0.5));
-    EXPECT_DOUBLE_EQ(10.5, multiply.apply(7, 1.5));
-
-    jbi::arithmetic_operation divide = jbi::arithmetic_operation::division();
-
-    EXPECT_EQ(3, divide.apply(7, 2));
-    EXPECT_DOUBLE_EQ(3.0, divide.apply(7.5, 2.5));
-    EXPECT_DOUBLE_EQ(3.5, divide.apply(7.0, 2));
-
-    jbi::arithmetic_operation exponentiate = jbi::arithmetic_operation::exponentiation();
-
-    EXPECT_EQ(49, exponentiate.apply(7, 2));
-    EXPECT_DOUBLE_EQ(2.25, exponentiate.apply(1.5, 2.0));
-    EXPECT_DOUBLE_EQ(8.0, exponentiate.apply(64, 0.5));
+    EXPECT_EQ(49, exponentiation.apply(7, 2));
+    EXPECT_DOUBLE_EQ(2.25, exponentiation.apply(1.5, 2.0));
+    EXPECT_DOUBLE_EQ(8.0, exponentiation.apply(64, 0.5));
 }
 
-TEST(arithmetic_operation_tests, division_by_zero_test)
+TEST_F(arithmetic_operation_tests, division_by_zero_test)
 {
-    jbi::arithmetic_operation division = jbi::arithmetic_operation::division();
-
     EXPECT_THROW(division.apply(1, 0), jbi::arithmetic_exception);
     EXPECT_NO_THROW(division.apply(1.0, 0));
     EXPECT_NO_THROW(division.apply(1, 0.0));
 }
 
-TEST(arithmetic_operation_tests, equality_operators_test)
+TEST_F(arithmetic_operation_tests, equality_operators_test)
 {
-    EXPECT_TRUE(jbi::arithmetic_operation::addition() == jbi::arithmetic_operation::addition());
-    EXPECT_FALSE(jbi::arithmetic_operation::addition() != jbi::arithmetic_operation::addition());
+    EXPECT_TRUE(addition == addition);
+    EXPECT_FALSE(addition != addition);
 
-    EXPECT_TRUE(jbi::arithmetic_operation::addition() != jbi::arithmetic_operation::subtraction());
-    EXPECT_FALSE(jbi::arithmetic_operation::addition() == jbi::arithmetic_operation::subtraction());
+    EXPECT_TRUE(addition != subtraction);
+    EXPECT_FALSE(addition == subtraction);
 
-    EXPECT_TRUE(jbi::arithmetic_operation::addition() != jbi::arithmetic_operation::multiplication());
-    EXPECT_FALSE(jbi::arithmetic_operation::addition() == jbi::arithmetic_operation::multiplication());
+    EXPECT_TRUE(addition != multiplication);
+    EXPECT_FALSE(addition == multiplication);
 
-    EXPECT_TRUE(jbi::arithmetic_operation::addition() != jbi::arithmetic_operation::division());
-    EXPECT_FALSE(jbi::arithmetic_operation::addition() == jbi::arithmetic_operation::division());
+    EXPECT_TRUE(addition != division);
+    EXPECT_FALSE(addition == division);
 
-    EXPECT_TRUE(jbi::arithmetic_operation::addition() != jbi::arithmetic_operation::exponentiation());
-    EXPECT_FALSE(jbi::arithmetic_operation::addition() == jbi::arithmetic_operation::exponentiation());
+    EXPECT_TRUE(addition != exponentiation);
+    EXPECT_FALSE(addition == exponentiation);
 }
 
-TEST(arithmetic_operation_tests, from_symbol_test)
+TEST_F(arithmetic_operation_tests, from_symbol_test)
 {
-    EXPECT_EQ(jbi::arithmetic_operation::addition(), jbi::arithmetic_operation::from_symbol('+'));
-    EXPECT_EQ(jbi::arithmetic_operation::subtraction(), jbi::arithmetic_operation::from_symbol('-'));
-    EXPECT_EQ(jbi::arithmetic_operation::multiplication(), jbi::arithmetic_operation::from_symbol('*'));
-    EXPECT_EQ(jbi::arithmetic_operation::division(), jbi::arithmetic_operation::from_symbol('/'));
-    EXPECT_EQ(jbi::arithmetic_operation::exponentiation(), jbi::arithmetic_operation::from_symbol('^'));
+    EXPECT_EQ(addition, jbi::arithmetic_operation::from_symbol('+'));
+    EXPECT_EQ(subtraction, jbi::arithmetic_operation::from_symbol('-'));
+    EXPECT_EQ(multiplication, jbi::arithmetic_operation::from_symbol('*'));
+    EXPECT_EQ(division, jbi::arithmetic_operation::from_symbol('/'));
+    EXPECT_EQ(exponentiation, jbi::arithmetic_operation::from_symbol('^'));
 
     EXPECT_THROW(jbi::arithmetic_operation::from_symbol('%'), jbi::argument_exception);
 }
